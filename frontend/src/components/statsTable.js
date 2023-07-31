17c014be068cbe5aa3b60dc3cd65ecccd54b29d7
Jenn Mohr (TMNA) <jennifer@bpmohr.com>
2023-07-30 21:25:27 -0400

    Update statsTable.js

--- frontend/src/components/statsTable.js
+++ frontend/src/components/statsTable.js
@@ -14,11 +14,11 @@ function StatsTable(props) {
       </thead>
       <tbody>
         <tr>
-          <td>{(props.stats && props.stats["PASS"]) || "-"}</td>
-          <td>{(props.stats && props.stats["SHOT"]) || "-"}</td>
-          <td>{(props.stats && props.stats["REB"]) || "-"}</td>
-          <td>{(props.stats && props.stats["FOUL"]) || "-"}</td>
-          <td>{(props.stats && props.stats["TO"]) || "-"}</td>
+          <td>{(props.stats && props.stats["PASS"]) || "0"}</td>
+          <td>{(props.stats && props.stats["SHOT"]) || "0"}</td>
+          <td>{(props.stats && props.stats["REB"]) || "0"}</td>
+          <td>{(props.stats && props.stats["FOUL"]) || "0"}</td>
+          <td>{(props.stats && props.stats["TO"]) || "0"}</td>
         </tr>
       </tbody>
     </Table>
